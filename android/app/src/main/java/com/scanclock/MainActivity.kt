68e2f893c5cd1f0ce2eff323fe1f4b29923dea3c
wahyukmr <maryu9229@gmail.com>
2024-05-12 13:44:15 +0700

    [update] 🚀 resolved issue in react-native-vision-camera and added splash screen

--- android/app/src/main/java/com/scanclock/MainActivity.kt
+++ android/app/src/main/java/com/scanclock/MainActivity.kt
@@ -5,10 +5,12 @@ import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 import android.os.Bundle;
+import com.zoontek.rnbootsplash.RNBootSplash;
 
 class MainActivity : ReactActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(null)
+    RNBootSplash.init(this, R.style.BootTheme) // ⬅️ initialize the splash screen
+    super.onCreate(null) // or super.onCreate(null) with react-native-screens
   }
 
   /**
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import {StyleSheet, Text} from 'react-native';
+import Bootsplash from 'react-native-bootsplash';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 const App = () => {
   return (
     <SafeAreaProvider>
-      <NavigationContainer>
+      <NavigationContainer onReady={() => Bootsplash.hide()}>
         <Text>App</Text>
       </NavigationContainer>
     </SafeAreaProvider>
