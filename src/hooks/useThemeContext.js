bbd94a959d3a3766154298efaadbc7c562698950
wahyukmr <maryu9229@gmail.com>
2024-05-31 09:30:05 +0700

    [update]  🚀add theme color style

--- src/hooks/useThemeContext.js
+++ src/hooks/useThemeContext.js
@@ -3,14 +3,17 @@ import {useColorScheme} from 'react-native';
 import {DARK_MODE, LIGHT_MODE} from '../constants';
 import ThemeContext from '../contexts/ThemeProvider';
 
-export const useThemeContext = () => {
+export const useThemeContext = styleSheets => {
   const {theme, setTheme} = useContext(ThemeContext);
   const systemColorScheme = useColorScheme();
 
   const themePreferences = (theme || systemColorScheme) ?? 'light';
   const themeColors = themePreferences === 'dark' ? DARK_MODE : LIGHT_MODE;
 
+  const styles = styleSheets(themeColors);
+
   return {
+    styles,
     themeColors,
     themePreferences,
     isSystemTheme: !theme,
