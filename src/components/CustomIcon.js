148c2956ac5ceee1d1589d4df3cea9a44a6cd150
wahyukmr <maryu9229@gmail.com>
2024-06-12 11:37:47 +0700

    [update] 🚀create dynamic custom icons

--- src/components/CustomIcon.js
+++ src/components/CustomIcon.js
@@ -1,17 +1,8 @@
 import React from 'react';
 import VectorIcon from 'react-native-vector-icons/MaterialCommunityIcons';
-import {useThemeContext} from '../hooks/useThemeContext';
-import {customIconStyles} from './CustomIcon.styles';
 
-const CustomIcon = ({name, size, color}) => {
-  const {styles} = useThemeContext(customIconStyles);
-  return (
-    <VectorIcon
-      name={name}
-      size={size}
-      color={color === 'primary' ? styles.iconPrimary : styles.iconSecondary}
-    />
-  );
-};
+const CustomIcon = ({name, size, color}) => (
+  <VectorIcon name={name} size={size} color={color} />
+);
 
 export default CustomIcon;
--- src/components/CustomIcon.styles.js
+++
@@ -1,7 +0,0 @@
-import {StyleSheet} from 'react-native';
-
-export const customIconStyles = themeColors =>
-  StyleSheet.create({
-    iconPrimary: themeColors.text200,
-    iconSecondary: themeColors.accent200,
-  });
