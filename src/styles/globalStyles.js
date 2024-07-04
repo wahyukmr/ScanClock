53add08eb6cbed4609a80b8e05ed6eb75850960a
wahyukmr <maryu9229@gmail.com>
2024-07-04 12:40:16 +0700

    [delete] ⛔deleting global styles

--- src/styles/globalStyles.js
+++
@@ -1,7 +0,0 @@
-import {StyleSheet} from 'react-native';
-
-export const globalStyles = themeColors =>
-  StyleSheet.create({
-    statusBarColor: themeColors.bg100,
-    backgroundColor: themeColors.bg100,
-  });
--- src/styles/index.js
+++
@@ -1,4 +0,0 @@
-import {authenticationStyles} from './authenticationStyles';
-import {globalStyles} from './globalStyles';
-
-export {authenticationStyles, globalStyles};
