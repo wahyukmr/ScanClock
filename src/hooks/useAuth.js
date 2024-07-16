b013f843b3940e97d3f4fb5a87638b0c4513664d
wahyukmr <maryu9229@gmail.com>
2024-07-16 11:57:48 +0700

    [update] 🚀Complete user authentication handling

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,32 +1,48 @@
-import {useState} from 'react';
-import {removeToken, storeToken} from '../services/tokenService';
+import {useReducer} from 'react';
+import {userService} from '../features/auth/services/userService';
+import initialState, {authReducer} from '../reducers/authReducer';
+import {credentialService} from '../services/credentialService';
+import {storageService} from '../services/storageService';
+import {tokenStorage} from '../services/tokenService';
 
 export const useAuth = () => {
-  const [userInfo, setUserInfo] = useState(null);
+  const [state, dispatch] = useReducer(authReducer, initialState);
 
-  console.log('userInfo', userInfo);
-
-  const login = async userData => {
+  const initializeAuth = async () => {
+    dispatch({type: 'SET_LOADING', payload: true});
     try {
-      await storeToken(userData.token);
-      if (userInfo !== userData) {
-        setUserInfo(userData);
+      const token = await tokenStorage.getToken();
+      if (token) {
+        console.log('ada token,');
+        const userData = await userService.fetchUserData();
+        dispatch({type: 'SET_USER', payload: userData});
+      } else {
+        dispatch({type: 'SET_LOADING', payload: false});
       }
     } catch (error) {
-      console.error('Error during login:', error.message);
+      dispatch({type: 'SET_ERROR', payload: error.message});
     }
+  };
+
+  const login = userData => {
+    dispatch({type: 'SET_USER', payload: userData});
   };
 
   const logout = async () => {
     try {
-      await removeToken();
-      setUserInfo(null);
+      dispatch({type: 'SET_LOADING', payload: true});
+      storageService.removeAllItemFromStorage();
+      await credentialService.removeCredentials();
+      await tokenStorage.removeToken();
+      await userService.clearUserData();
+      dispatch({type: 'LOGOUT'});
     } catch (error) {
-      console.error('Error during logout:', error);
+      dispatch({
+        type: 'SET_ERROR',
+        payload: `Error during logout: ${error.message}`,
+      });
     }
   };
 
-  // get user
-
-  return {userInfo, login, logout};
+  return {...state, login, logout, initializeAuth};
 };
