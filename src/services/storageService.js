e357faad523675c03ce1d15c7f2ac715a0f3d7dd
wahyukmr <maryu9229@gmail.com>
2024-07-16 12:01:43 +0700

    [update] 🚀updated token and credential export

--- src/services/storageService.js
+++ src/services/storageService.js
@@ -2,10 +2,16 @@ import {MMKV} from 'react-native-mmkv';
 
 const storage = new MMKV();
 
-export const setItemToStorage = (key, value) => {
-  storage.set(key, value);
-};
+export const storageService = {
+  setItemToStorage: (key, value) => {
+    storage.set(key, value);
+  },
 
-export const getItemFromStorage = key => {
-  return storage.getString(key);
+  getItemFromStorage: key => {
+    return storage.getString(key);
+  },
+
+  removeAllItemFromStorage: () => {
+    storage.clearAll();
+  },
 };
--- src/services/tokenService.js
+++ src/services/tokenService.js
@@ -1,50 +1,28 @@
 import EncryptedStorage from 'react-native-encrypted-storage';
 import {STORAGE_KEY} from '../constants';
 
-export const storeToken = async token => {
-  try {
-    await EncryptedStorage.setItem(STORAGE_KEY.TOKEN, token);
-  } catch (error) {
-    throw new Error("Can't store token: " + error.message);
-  }
-};
+export const tokenStorage = {
+  storeToken: async token => {
+    try {
+      await EncryptedStorage.setItem(STORAGE_KEY.TOKEN, token);
+    } catch (error) {
+      throw new Error("Can't store token: " + error.message);
+    }
+  },
 
-export const getToken = async () => {
-  try {
-    return await EncryptedStorage.getItem(STORAGE_KEY.TOKEN);
-  } catch (error) {
-    throw new Error('Error getting token', error.message);
-  }
-};
+  getToken: async () => {
+    try {
+      return await EncryptedStorage.getItem(STORAGE_KEY.TOKEN);
+    } catch (error) {
+      throw new Error('Error getting token: ', error.message);
+    }
+  },
 
-export const removeToken = async () => {
-  try {
-    await EncryptedStorage.removeItem(STORAGE_KEY.TOKEN);
-  } catch (error) {
-    throw new Error('Error removing token', error.message);
-  }
-};
-
-export const setCredentials = async (email, password) => {
-  try {
-    await EncryptedStorage.setItem(STORAGE_KEY.CREDENTIALS, {email, password});
-  } catch (error) {
-    throw new Error('Error saving credentials user', error.message);
-  }
-};
-
-export const getCredentials = async () => {
-  try {
-    return await EncryptedStorage.getItem(STORAGE_KEY.CREDENTIALS);
-  } catch (error) {
-    throw new Error('Error getting credentials user', error.message);
-  }
-};
-
-export const removeCredentials = async () => {
-  try {
-    await EncryptedStorage.removeItem(STORAGE_KEY.CREDENTIALS);
-  } catch (error) {
-    throw new Error('Error removing credentials user', error.message);
-  }
+  removeToken: async () => {
+    try {
+      await EncryptedStorage.removeItem(STORAGE_KEY.TOKEN);
+    } catch (error) {
+      throw new Error('Error removing token: ', error.message);
+    }
+  },
 };
