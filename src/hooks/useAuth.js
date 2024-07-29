53cd2860f1a1cc0a43ba4cfeabd3c0f5ddf3e6d6
wahyukmr <maryu9229@gmail.com>
2024-07-29 11:38:16 +0700

    [update] 🚀implementation of tanstack query

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,48 +1,52 @@
-import {useReducer} from 'react';
-import initialState, {authReducer} from '../features/auth/reducers/authReducer';
+import {useQueryClient} from '@tanstack/react-query';
+import {useState} from 'react';
 import {userService} from '../features/auth/services/userService';
 import {credentialService} from '../services/credentialService';
 import {storageService} from '../services/storageService';
 import {tokenStorage} from '../services/tokenService';
+import {delay} from '../utils/delay';
 
 export const useAuth = () => {
-  const [state, dispatch] = useReducer(authReducer, initialState);
+  const [user, setUser] = useState(null);
+  const queryClient = useQueryClient();
 
   const initializeAuth = async () => {
-    dispatch({type: 'SET_LOADING', payload: true});
     try {
       const token = await tokenStorage.getToken();
-      if (token) {
-        console.log('ada token,');
-        const userData = await userService.fetchUserData();
-        dispatch({type: 'SET_USER', payload: userData});
-      } else {
-        dispatch({type: 'SET_LOADING', payload: false});
+      const userData = await userService.getUserData();
+
+      if (token && userData) {
+        setUser(userData);
       }
     } catch (error) {
-      dispatch({type: 'SET_ERROR', payload: error.message});
+      throw new Error(error);
     }
   };
 
-  const login = userData => {
-    dispatch({type: 'SET_USER', payload: userData});
+  const login = async userData => {
+    try {
+      if (userData) {
+        await userService.storeUserData(userData);
+        setUser(userData);
+      }
+    } catch (error) {
+      throw new Error(error);
+    }
   };
 
   const logout = async () => {
     try {
-      dispatch({type: 'SET_LOADING', payload: true});
       storageService.removeAllItemFromStorage();
+      queryClient.clear();
       await credentialService.removeCredentials();
       await tokenStorage.removeToken();
       await userService.clearUserData();
-      dispatch({type: 'LOGOUT'});
+      await delay(500);
+      setUser(null);
     } catch (error) {
-      dispatch({
-        type: 'SET_ERROR',
-        payload: `Error during logout: ${error.message}`,
-      });
+      throw new Error(error);
     }
   };
 
-  return {...state, login, logout, initializeAuth};
+  return {user, login, logout, initializeAuth};
 };
