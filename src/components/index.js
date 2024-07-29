074db010c4016f00fe844ae1bdc15085d3f5e2a1
wahyukmr <maryu9229@gmail.com>
2024-07-29 11:31:42 +0700

    [update] 🚀handle exports components

--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,15 @@
+import CustomHeaderTitle from './CustomHeaderTitle ';
 import CustomIcon from './CustomIcon';
 import CustomLoading from './CustomLoading';
 import DynamicModalWrapper from './DynamicModalWrapper';
-import ErrorModalContent from './ErrorModalContent';
 import ModalActionBtn from './ModalActionBtn';
+import StatusModalContent from './StatusModalContent';
 
 export {
+  CustomHeaderTitle,
   CustomIcon,
   CustomLoading,
   DynamicModalWrapper,
-  ErrorModalContent,
   ModalActionBtn,
+  StatusModalContent,
 };
